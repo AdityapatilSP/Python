673f7c09e5a1402a101dcbd0a27cbcd6901cf02a
Aditya Santosh patil <156800442+AdityapatilSP@users.noreply.github.com>
2024-12-19 10:21:37 +0530

    Delete DAY 1.py

--- DAY 1.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to band name generator")
-A = input("Whats the name of city you grew up in?") # using the input function we take an input in string from the users
-B = input("Whats your pet's name?") # same here using a variable with input function to take input and store it in variable
-print("your band name is", A + B) #using string concatenation to add two strings we create the band name
-
-#This is a simple band name generator code for beginners to understand python
